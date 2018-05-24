c7d7eca791f95012c82b051e45f04b196c37668a
Arkham05 <31805904+Arkham05@users.noreply.github.com>
2018-05-24 13:52:05 -0500

    PrimerPush
    base del proyecto

---
+++ FISC/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Sistema de Salas de Cómputo</title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="./css/uaemex.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	</head>
+	<body>
+		<div class="container">
+			<div id="banner">
+				<a href="http://localhost/FISC/Index.php">
+					<img id="banner-img" src="./resources/bannerFI.png">
+				</a>
+			</div>
+			<div id="contenido">
+				<div class="row">
+					<div class="col-sm-*"></div>
+					<div class="col-sm-*"></div>
+					<div class="col-sm-*"></div>
+				</div>
+				<img id="contenidofilinkimg" src="./resources/cabecera.png">
+				<img id="contenidobanpreimg" src="./resources/banprestamo.png">
+			</div>
+			<div id="footer">
+			</div>
+		</div>
+	</body>
+</html>
