b9f79e24e7af9f346df65dcb9b3ba4a9f97a624a
Arkham <jackripper96@hotmail.com>
2018-05-24 15:47:22 -0500

    resuelto doble index
    se guardo el index en su posicion normal
    index23 es basura
    se agregaron imagenes de menu de amdin

--- FISC/views/index.php
+++ FISC/views/index.php
@@ -4,11 +4,35 @@
 		<title>Sistema de Salas de Cómputo</title>
 		<meta charset="UTF-8">
 		<link rel="stylesheet" type="text/css" href="../css/uaemex.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	</head>
 	<body>
 		<div class="container">
 			<div id="banner">
-				<img src="resources/bannerFI.png">
+				<a href="http://localhost/FISC/Index.php">
+					<img id="bannerimg" src="../resources/bannerFI.png">
+				</a>
+			</div>
+			<div id="contenido">
+				<img id="contenidofilinkimg" src="../resources/cabecera.png">
+				<div class="row">
+					<div class="col-sm-1"></div>
+					<a href="http://localhost/FISC/Index.php">
+						<div id="contenidoado1" class="col-sm-3">
+							<img id="contenidoado1img" src="../resources/solicitud.png">
+						</div>
+					</a>
+					<a href="http://localhost/FISC/Index.php">
+						<div id="contenidoado2" class="col-sm-3">
+							<img id="contenidoado2img" src="../resources/bd.png">
+						</div>
+					</a>
+					<a href="http://localhost/FISC/Index.php">
+						<div id="contenidoado3" class="col-sm-3">
+							<img id="contenidoado3img" src="../resources/reportes.png">
+						</div>
+					</a>
+				</div>
 			</div>
 			<div id="footer">
 			</div>
