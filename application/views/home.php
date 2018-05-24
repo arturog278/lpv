1cf1677c2e132b2ec8700f2423589025659d13e6
Arturo <arturog278@gmail.com>
2018-05-24 18:34:43 -0500

    Cambio en redireccionamiento de imagenes.

--- application/views/home.php
+++ application/views/home.php
@@ -2,7 +2,7 @@
   <img id="contenidofilinkimg" src="<?php echo asset_url('images/cabecera.png'); ?>">
   <div class="row">
     <div class="col-sm-1"></div>
-    <a href="http://localhost/lpv">
+    <a href="http://localhost:8080/lpv">
       <div id="contenidoado1" class="col-sm-3">
         <img id="contenidoado1img" src="<?php echo asset_url('images/solicitud.png'); ?>">
         <br/><br/>
@@ -12,7 +12,7 @@
         </div>
       </div>
     </a>
-    <a href="http://localhost/lpv">
+    <a href="http://localhost:8080/lpv">
       <div id="contenidoado2" class="col-sm-3">
         <img id="contenidoado2img" src="<?php echo asset_url('images/bd.png'); ?>">
         <br/><br/>
@@ -21,7 +21,7 @@
         </div>
       </div>
     </a>
-    <a href="http://localhost/lpv">
+    <a href="http://localhost:8080/lpv">
       <div id="contenidoado3" class="col-sm-3">
         <img id="contenidoado3img" src="<?php echo asset_url('images/reportes.png'); ?>">
         <br/><br/>
