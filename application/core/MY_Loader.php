3bd9125f65abb9edfea8116ddf28e85f58fe33de
Arturo <arturog278@gmail.com>
2018-05-24 18:28:42 -0500

    Errores de PHP resueltos

--- application/core/MY_Loader.php
+++ application/core/MY_Loader.php
@@ -6,9 +6,9 @@
 class MY_Loader extends CI_Loader {
     public function template($template_name, $vars = array(), $return = FALSE)
     {
-        $content  = $this->view('templates/header', $vars, $return); // header
-        $content .= $this->view($template_name, $vars, $return); // view
-        $content .= $this->view('templates/footer', $vars, $return); // footer
+        $content = $this->view('templates/header', $vars, $return); // header
+        $content = $this->view($template_name, $vars, $return); // view
+        $content = $this->view('templates/footer', $vars, $return); // footer
 
         if ($return)
         {
