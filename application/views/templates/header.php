382fdb8d3408edf58c94dce0686a1d697b5ee564
Arturo <arturog278@gmail.com>
2018-05-24 20:11:31 -0500

    Arreglado header.php

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,13 +3,10 @@
 	<head>
 		<title>Sistema de Salas de Cómputo</title>
 		<meta charset="UTF-8">
-<<<<<<< HEAD
 		<link href="<?php echo asset_url('css/uaemex.css'); ?>" rel="stylesheet" type="text/css">
 			<link href="<?php echo asset_url('css/login.css'); ?>" rel="stylesheet" type="text/css">
-=======
 		<link href="<?php echo asset_url('css/admin.css'); ?>" rel="stylesheet" type="text/css">
 		<link href="<?php echo asset_url('css/adminSol.css'); ?>" rel="stylesheet" type="text/css">
->>>>>>> a68111edd79ba56e708fbae31c921756c24ea526
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	</head>
 	<body>
