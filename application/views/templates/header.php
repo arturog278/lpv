cba3cf8f96763e0d2931b9b1af782231ef5602e6
valentinmedi9 <valentinmedi@hotmail.com>
2018-05-25 01:37:46 -0500

    lol

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,11 +3,16 @@
 	<head>
 		<title>Sistema de Salas de Cómputo</title>
 		<meta charset="UTF-8">
+<<<<<<< current
 		<link href="<?php echo asset_url('css/login.css'); ?>" rel="stylesheet" type="text/css">
 		<link href="<?php echo asset_url('css/admin.css'); ?>" rel="stylesheet" type="text/css">
 		<link href="<?php echo asset_url('css/adminSol.css'); ?>" rel="stylesheet" type="text/css">
 		<link href="<?php echo asset_url('css/adminbd.css'); ?>" rel="stylesheet" type="text/css">
 		<link href="<?php echo asset_url('css/adminRep.css'); ?>" rel="stylesheet" type="text/css">
+=======
+		<link href="<?php echo asset_url('css/uaemex.css'); ?>" rel="stylesheet" type="text/css">
+			<link href="<?php echo asset_url('css/login.css'); ?>" rel="stylesheet" type="text/css">
+>>>>>>> before discard
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	</head>
 	<body>
