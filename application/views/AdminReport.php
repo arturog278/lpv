cf2d49330250f8763cbd276022cfdbb52a3d13d1
Arkham <jackripper96@hotmail.com>
2018-05-24 22:03:53 -0500

    adminreport
    creacion de vista report

---
+++ application/views/AdminReport.php
@@ -0,0 +1,38 @@
+<div id="contenido">
+  <img id="contenidofilinkimg" src="<?php echo asset_url('images/cabecera.png'); ?>">
+  <div id="contenidoregresar">
+    <button type="button" class="btn btn-warning">Regresar</button>
+  </div>
+  <br/><br/>
+  <div id="contenidorep" class="row">
+    <div id="contenidorepnor" class="col-sm-6">
+      <span>Reportes Normales</span>
+    </div>
+    <div id="contenidorepesp" class="col-sm-6">
+      <span>Reporte Especial</span>
+    </div>
+  </div>
+  <div id="contenidotabsol" class="row">
+    <div class="col-sm-6">
+      <div class="table-responsive">
+        <table class="table">
+          <tr><td><button type="button" class="btn btn-warning">Regresar</button></td></tr>
+        </table>
+      </div>
+    </div>
+    <div class="col-sm-6">
+      <div class="table-responsive">
+        <table class="table">
+          <thead class="thead-light">
+            <tr>
+              <th>Reporte Especial</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr><td><button type="button" class="btn btn-warning">Reporte Especial Asistido</button></td></tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
--- ficomp.sql
+++ ficomp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-05-2018 a las 04:53:42
+-- Tiempo de generación: 25-05-2018 a las 05:03:07
 -- Versión del servidor: 10.1.31-MariaDB
 -- Versión de PHP: 7.2.3
 
