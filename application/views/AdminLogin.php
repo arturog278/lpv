a36b53cd416539cd27e7ebe85564b5fe60fc4470
valentinmedi9 <valentinmedi@hotmail.com>
2018-05-24 21:05:38 -0500

    AdminLogin

--- application/views/AdminLogin.php
+++ application/views/AdminLogin.php
@@ -8,26 +8,16 @@
       you can substitue the span of reauth email for a input with the email and
       include the remember me checkbox
       -->
-      <div class="row">
-        <div class="col-sm-1"></div>
+
         <div class="card card-container">
-            <!-- <img class="profile-img-card" src="//lh3.googleusercontent.com/-6V8xOA6M7BA/AAAAAAAAAAI/AAAAAAAAAAA/rzlHcD0KYwo/photo.jpg?sz=120" alt="" /> -->
+            <h2>Ingreso al sistema</h2>
             <p id="profile-name" class="profile-name-card"></p>
             <form class="form-signin">
                 <span id="reauth-email" class="reauth-email"></span>
-                <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-                <div id="remember" class="checkbox">
-                    <label>
-                        <input type="checkbox" value="remember-me"> Remember me
-                    </label>
-                </div>
-                <button class="btn btn-lg btn-primary btn-block btn-signin" type="submit">Sign in</button>
+                <input type="cuenta" id="inputEmail" class="form-control" placeholder="Numero de cuenta" required autofocus>
+                <input type="contraseña" id="inputPassword" class="form-control" placeholder="Contraseña" required>
+                <button class="btn btn-lg btn-primary btn-block btn-signin" type="submit">Ingresar</button>
             </form><!-- /form -->
-            <a href="#" class="forgot-password">
-                Forgot the password?
-            </a>
-        </div><!-- /card-container -->
-            </div>
 
-          </div>
+       </div>  <!-- /card-container -->
+    </div>
