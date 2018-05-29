552a250d88f106407e6e5b4de5639db728c5b808
sebaskyclad <sebasvap20@gmail.com>
2018-05-29 17:14:18 -0500

    cambios a AdminBD y  Alumno_model

--- application/models/Alumno_model.php
+++ application/models/Alumno_model.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Admin_model extends CI_Model {
+class Alumno_model extends CI_Model {
 
     public function __constructor()
     {
@@ -8,24 +8,44 @@ class Admin_model extends CI_Model {
     }
 
     public function insert($var){
-      $this->db->insert('admin',$var);
+      $this->db->insert('alumno',$var);
     }
 
     public function delete($var){
-      $this->db->delete('admin', array('adminID' => $var));
+      $this->db->delete('alumno', array('cuenta' => $var));
     }
 
-    public function get_credenciales($user,$pass){
-      $this->db->select('adminID');
-      $this->db->where('usuario',$user);
-      $this->db->where('pass',$pass);
+    public function update($cuenta, $var){
+      $this->db->where('cuenta',$cuenta);
+      $this->db->update('alumno',$var);
+    }
+
+    public function get_credenciales($cuenta){
+      $this->db->select('nombre');
+      $this->db->where('cuenta',$user);
       $res = $this->db->get('admin');
       $data = $res->result_array();
       return $data[0];
     }
 
-    public function get_by_usuario($var)
+    public function getall()
     {
-
+      $res = $this->db->get('alumno');
+      $data = $res->result_array();
+      return $data;
+    }
+    public function getbyName($nombre,$apellidop,$apellidom)
+    {
+      $this->db->where('nombre',$nombre);
+      $this->db->where('apellidop',$apellidop);
+      $this->db->where('apellidom',$apellidom);
+      $data = $res->result_array();
+      return $data;
+    }
+    public function getbyCuenta($cuenta)
+    {
+      $this->db->where('cuenta',$cuenta);
+      $data = $res->result_array();
+      return $data;
     }
 }
