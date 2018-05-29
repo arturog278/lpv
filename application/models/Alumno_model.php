6fdd5196f2fbe6e1b493019e6fd6cadc2592efe7
sebaskyclad <sebasvap20@gmail.com>
2018-05-29 16:49:55 -0500

    Alumno model

---
+++ application/models/Alumno_model.php
@@ -0,0 +1,31 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Admin_model extends CI_Model {
+
+    public function __constructor()
+    {
+        parent::__construct();
+    }
+
+    public function insert($var){
+      $this->db->insert('admin',$var);
+    }
+
+    public function delete($var){
+      $this->db->delete('admin', array('adminID' => $var));
+    }
+
+    public function get_credenciales($user,$pass){
+      $this->db->select('adminID');
+      $this->db->where('usuario',$user);
+      $this->db->where('pass',$pass);
+      $res = $this->db->get('admin');
+      $data = $res->result_array();
+      return $data[0];
+    }
+
+    public function get_by_usuario($var)
+    {
+
+    }
+}
