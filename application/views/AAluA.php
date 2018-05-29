e6c6723281f815ae6b25932f97980049a6cf2712
Arkham <jackripper96@hotmail.com>
2018-05-29 16:26:49 -0500

    wfer
    ferfer

---
+++ application/views/AAluA.php
@@ -0,0 +1,10 @@
+<div id="contenido">
+  <img id="contenidofilinkimg" src="<?php echo asset_url('images/cabecera.png'); ?>">
+  <div id="contenidoreg">
+    <a href="http://localhost:8080/lpv/AdminBD">
+      <button type="button" class="btn btn-warning">Regresar</button>
+    </a>
+  </div>
+  <br/><br/>
+  </div>
+</div>
