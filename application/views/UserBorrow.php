936294e905d3f9622da0b78445460902781376a7
valentinmedi9 <valentinmedi@hotmail.com>
2018-05-29 20:52:22 -0500

    BAIA BAIA

--- application/views/UserBorrow.php
+++ application/views/UserBorrow.php
@@ -5,9 +5,8 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
   <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+  <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
   <div id="contenidoreg">
     <a href="http://localhost:8080/lpv/AlumnoMenu">
       <button type="button" class="btn btn-warning">Regresar</button>
@@ -26,6 +25,7 @@
     </div>
   </div>
   <body>
+      <div class="card card-container">
       <div class="panel-group" id="accordion">
       <br>
       <div class="panel panel-default">
@@ -52,6 +52,6 @@
                   </div>
 
 
-
+                </div>
                 </body>
 </div>
